2edf33ab10be008314476f641a9aeb1d4efb972d
jawad <jawad.turk@hotmail.com>
2018-05-21 16:27:20 -0500

    add jquery code and json

--- web/scripts/DynamicRestaurants.js
+++ web/scripts/DynamicRestaurants.js
@@ -21,11 +21,81 @@ $(function () {
 });
 
 function retrieveRestaurants(cuisineId,cityId) {
-    $.get('restaurants', {
+    $('#loader').show();
+    $.get('dynamicRestaurants', {
         cuisineType : cuisineId,
         cities:cityId
     }, function(responseText) {
         console.log(responseText)
+        responseText = JSON.parse(responseText);
+        $("#myElementsContainer").empty();
+        $.each(responseText, function(i, item) {
 
+            var divColor = ($("<div>"));
+            divColor.addClass("agile-product-text");
+            divColor.addClass("agile-product-text2");
+            if (i%2===0) {
+                divColor.removeClass("agile-product-text2");
+            }
+
+            console.log(i);
+            $("#myElementsContainer")
+                .append($("<div>")
+                    .addClass("col-xs-6")
+                    .addClass("col-sm-4")
+                    .addClass("product-grids")
+                    .append($("<div>")
+                        .addClass("flip-container")
+
+                        .append($("<div>")
+                            .addClass("flipper")
+                            .addClass("agile-products")
+                            .append($("<div>")
+                                .addClass("front")
+                                .append($("<img>")
+                                    .addClass("img-responsive")
+                                    .attr("src","/resources/images/"+item.thumbnailImage)
+                                    .attr("alt","")
+                                )
+
+                                .append(divColor
+                                .append($("<h5>").text(item.name))
+                                )
+
+
+                            )
+                            .append($("<div>")
+                                .addClass("back")
+
+                                .append($("<h4>").text(item.name))
+                                .append($("<p>").text(item.address))
+                                .append($("<p>").text(item.phoneNo))
+                                .append($("<a>")
+                                    .addClass("")
+                                    .text("More")
+                                    .attr("href", "#")
+                                    .attr('id', item.id)
+                                    .attr('name', 'moreOption')
+
+                                )
+                            )
+                        )
+                    )
+
+                )
+        });
+        $("#myElementsContainer")
+            .append($("<div>").addClass("clearfix"));
+
+
+
+
+
+
+
+
+
+    }).always(function(){
+        $('#loader').hide();
     });
 }
